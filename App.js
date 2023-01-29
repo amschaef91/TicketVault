c0cff6daf5d028bb77346485e58de25ec43d2657
Anthony Schaefbauer <as745677@southeast.edu>
2023-01-29 16:19:03 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image, TextInput } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+const image = require('./assets/concert.png')
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
 
 export default function App() {
+
+  const ticketPrice = 99.99;
+  const [numTicket, setTickets] = useState(0);
+  const [text, setText] = useState(' ');
+  const total = ticketPrice * numTicket
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Ticket Vault</Text>
+      <TextInput style={styles.input} onChangeText={newTicket => setTickets(newTicket)} 
+        placeholder='Number of Tickets'>
+      </TextInput>
+      <Pressable style={styles.button} onPress={newText => setText(`Ticket Cost: ${total.toFixed(2)}`) }>
+        <Text style={styles.buttonText}>Find the Cost</Text>
+      </Pressable>
+      <Text style={styles.output}>{text}</Text>
+      <Image source={image} style={styles.image} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +37,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    alignItems: 'center',
+    paddingVertical: 30,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    backgroundColor: '#f98b88'
+  },
+  buttonText: {
+    fontSize: 30
+  },
+  image: {
+    alignItems: 'center',
+    marginTop: 100,
+  },
+  input: {
+    fontSize: 30,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 30,
+  },
+  output: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 20,
   },
 });
